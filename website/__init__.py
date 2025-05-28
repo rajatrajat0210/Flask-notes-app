f37ea14a935e3c5e1bc52f076c460dc1a9936223
rajatrajat0210 <rajat.rajat0210@gmail.com>
2025-05-28 15:53:29 +0530

    Use environment variable for SECRET_KEY

--- website/__init__.py
+++ website/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'dhhdjdjdjdjd dhjdjjdd'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default-secret-key-for-dev')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
